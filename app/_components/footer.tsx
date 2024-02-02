b6b85aed7f20b79c486817c7dc191a6def388c36
JovemDevv <anacarolinesan18@gmail.com>
2024-02-01 23:07:00 -0300

    autenticando e colocando agendamento

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="bg-secondary2 w-full px-5 py-6">
       <p className="text-xs font-bold text-gray-400 opacity-75">
-        © 2023 Copyright FSW Barber
+        © 2023 Copyright BarberT
       </p>
     </div>
   );
